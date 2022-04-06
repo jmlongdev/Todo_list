6de6e2f676f81448cf0d243889e01c345067e83b
longj18 <maxsoccerssport@hotmail.com>
2022-04-05 23:01:32 -0400

    todo list nearly complete

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
 
 # Creates the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo-list.db'
@@ -13,8 +15,8 @@ db = SQLAlchemy(app)
 # this is the class that each creates object
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    time = db.Column(db.String(250), unique=True)
+    title = db.Column(db.String(250), nullable=False)
+
 
 
 db.create_all()
@@ -27,15 +29,15 @@ def home():
 
 
 # Adds a task to the database
-@app.route('/add_todo', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        new_todo = Todo(title=request.form['title'], time=request.form['time'])
+        new_todo = Todo(title=request.form['title'])
         db.session.add(new_todo)
         db.session.commit()
         return redirect(url_for('home'))
 
-    return render_template('add_todo.html')
+    return render_template('index.html')
 
 
 # Edits the tasks time to complete
@@ -44,12 +46,12 @@ def edit_todo():
     if request.method == 'POST':
         todo_id = request.form['id']
         todo_to_update = Todo.query.get(todo_id)
-        todo_to_update.time = request.form['time']
+        todo_to_update.title = request.form['title']
         db.session.commit()
         return redirect(url_for('home'))
     todo_id = request.args.get('id')
     todo_selected = Todo.query.get(todo_id)
-    return render_template('edit_todo.html', todo=todo_selected)
+    return render_template('edit.html', selected_todo=todo_selected)
 
 
 # Deletes the task
@@ -65,3 +67,9 @@ def delete_todo():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# <!--{% block scripts %}-->
+# <!--<script src="static/scripts/scripts.js"></script>-->
+# <!--{% endblock %}-->
+
