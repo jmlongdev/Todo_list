0fd6785f62798e09eb57b27a6a70e57ec523c747
longj18 <maxsoccerssport@hotmail.com>
2022-04-09 18:49:27 -0400

    finishing touches

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -17,8 +19,11 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), nullable=False)
 
-
 db.create_all()
+
+
+class EditTodo(FlaskForm):
+    title = StringField('Todo:')
 
 
 @app.route('/', methods=['GET'])
@@ -28,29 +33,15 @@ def home():
 
 
 # Adds a task to the database
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         new_todo = Todo(title=request.form['title'])
         db.session.add(new_todo)
         db.session.commit()
         return redirect(url_for('home'))
-
     return render_template('index.html')
 
-
-# Edits the tasks time to complete
-@app.route('/edit', methods=['GET', 'POST'])
-def edit_todo():
-    if request.method == 'POST':
-        todo_id = request.form['id']
-        todo_to_update = Todo.query.get(todo_id)
-        todo_to_update.title = request.form['title']
-        db.session.commit()
-        return redirect(url_for('home'))
-    todo_id = request.args.get('id')
-    todo_selected = Todo.query.get(todo_id)
-    return render_template('edit.html', selected_todo=todo_selected)
 
 
 # Deletes the task
@@ -67,8 +58,4 @@ def delete_todo():
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-# <!--{% block scripts %}-->
-# <!--<script src="static/scripts/scripts.js"></script>-->
-# <!--{% endblock %}-->
 
